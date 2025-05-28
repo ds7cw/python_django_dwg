494fd743f8abee9286af389a0adbc9c3e7068689
ds7cw <delyan.shivachev@hotmail.com>
2025-05-28 23:55:15 +0300

    Add htmx templates & views for testing

--- project/project/common/urls.py
+++ project/project/common/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
-from .views import index
+from .views import index, htmx_home, htmx_updated
 
 urlpatterns = [
     path('', index, name='index-page'),
+    path('htmx-home/', htmx_home, name='htmx-home'),
+    path('htmx-updated/', htmx_updated, name='htmx-updated'),
 ]
--- project/project/common/views.py
+++ project/project/common/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'common/index-page.html', context)
+
+def htmx_home(request):
+    context = {}
+    return render(request, 'htmx/htmx-home.html', context)
+
+def htmx_updated(request):
+    context = {}
+    return render(request, 'htmx/htmx-updated.html', context)
