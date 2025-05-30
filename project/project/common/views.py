fe463829328dd854a64c0049c06d3b388807349d
ds7cw <delyan.shivachev@hotmail.com>
2025-05-30 23:48:26 +0300

    Add work in progress prototype

--- project/project/common/views.py
+++ project/project/common/views.py
@@ -6,8 +6,17 @@ def index(request):
 
 def htmx_home(request):
     context = {}
+    request.session["guess"] = ""  # Reset guess on page load
     return render(request, 'htmx/htmx-home.html', context)
 
+
 def htmx_updated(request):
-    context = {}
-    return render(request, 'htmx/htmx-updated.html', context)
+
+    letter = request.GET.get("letter", "")
+    guess = request.session.get("guess", "")
+
+    if len(guess) < 5:  # Ensure max length is 5
+        guess += letter
+        request.session["guess"] = guess
+    
+    return render(request, "htmx/htmx-updated.html", {"guess": guess})
